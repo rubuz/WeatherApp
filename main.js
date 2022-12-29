1546091e3580c92eff9e51dea5a161fcc593be78
Matjaz Lovenjak <rubuz23@gmail.com>
2022-12-29 22:37:12 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,34 @@
+import "./style.css";
+import { getWeather } from "./weather";
+
+getWeather(10, 10, Intl.DateTimeFormat().resolvedOptions().timeZone)
+  .then(renderWeather)
+  .catch((e) => {
+    console.error(e);
+    alert("Error getting weather.");
+  });
+
+function renderWeather({ current, daily, hourly }) {
+  renderCurrentWeather(current);
+  // renderDailyWeather(daily);
+  // renderHourlyWeather(hourly);
+  document.body.classList.remove("blurred");
+}
+
+function setValue(selector, value, { parent = document } = {}) {
+  parent.querySelector(`[data-${selector}]`).textContent = value;
+}
+
+function getIconUrl
+
+const currentIcon = document.querySelector("[data-current-icon]");
+function renderCurrentWeather(current) {
+  currentIcon.src = getIconUrl(current.iconCode);
+  setValue("current-temp", current.currentTemp);
+  setValue("current-high", current.highTemp);
+  setValue("current-low", current.lowTemp);
+  setValue("current-fl-high", current.highFeelsLike);
+  setValue("current-fl-low", current.lowFeelsLike);
+  setValue("current-wind", current.windSpeed);
+  setValue("current-precip", current.precip);
+}
